1b2f06f31129099d10f7038cc4e368683fb5aa47
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2023-03-08 09:57:49 +0100

    second commit

--- command_app/models.py
+++ command_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from datetime import date
 # Create your models here.
 class Product(models.Model):
     label = models.CharField(max_length=50,default='')
@@ -13,8 +14,10 @@ class Product(models.Model):
 
 class Client(models.Model):
     name=models.CharField(max_length=50,default='')
-    birthDate=models.DateField(default='2000-01-01') 
+    birthDate=models.DateField(default= date(2000,1,1)) 
     #birthDate=models.DateField(default=timezone.now)
+    #specification of the many to many relationship between Client and Product
+    client_products=models.ManyToManyField(Product,through='Command',through_fields=('clt','prod'))
     class Meta:
         db_table='client'
     def __str__(self):
@@ -25,7 +28,6 @@ class Command(models.Model):
     clt=models.ForeignKey(Client,on_delete=models.CASCADE)
     date=models.DateField(default=timezone.now)
     quantity=models.PositiveSmallIntegerField(default=0)
-    amount=models.FloatField(default=0.0)
     class Meta:
         db_table='command'
     def __str__(self):
